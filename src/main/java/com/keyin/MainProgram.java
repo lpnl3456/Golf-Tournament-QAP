44f5bd31dfbadb539a272da1f560f32bbd6fe3a6
lpnl3456 <luke.peddle@keyin.com>
2024-11-25 15:16:31 -0330

    commit

--- src/main/java/com/keyin/MainProgram.java
+++ src/main/java/com/keyin/MainProgram.java
@@ -3,6 +3,7 @@ package com.keyin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+// hi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MainProgram {
